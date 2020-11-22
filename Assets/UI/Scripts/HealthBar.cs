b3ab7266b5c88bf2a008e8d5ed0de11212beffef
DiaymaOB <dymonofregmail.com>
2020-11-22 15:20:43 -0600

    UI
    UI implanted in Nathan's scene.  Package updated

---
+++ Assets/UI/Scripts/HealthBar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider healthSlider;
+    public Slider howlSlider;
+    public Gradient gradient;
+    public Image fill;
+
+    public void SetMaxHealth( int health)
+    {
+        healthSlider.maxValue = health;
+        healthSlider.value = health;
+
+        fill.color = gradient.Evaluate(1f);
+    }
+
+    public void SetMinHowl(int howl)
+    {
+        howlSlider.minValue = howl;
+        howlSlider.value = howl;
+    }
+
+    public void SetHealth(int health)
+    {
+        healthSlider.value = health;
+        fill.color = gradient.Evaluate(healthSlider.normalizedValue);
+    }
+
+
+    public void SetHowl(int howl)
+    {
+        howlSlider.value = howl;
+    }
+}
